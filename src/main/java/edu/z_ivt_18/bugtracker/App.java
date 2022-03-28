ba8ea49da9cb6b9371f0d1beaa4c961403fc940c
u3605689 <aoueau@rambler.ru>
2022-03-28 17:45:42 +0900

    Initial Maven project

---
+++ src/main/java/edu/z_ivt_18/bugtracker/App.java
@@ -0,0 +1,13 @@
+package edu.z_ivt_18.bugtracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
