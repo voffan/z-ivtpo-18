3879d494ae7e5449aabd01ccd6a3c21be89d31b2
u3605689 <aoueau@rambler.ru>
2022-04-07 16:06:03 +0900

    Change default look and feel.

--- src/main/java/edu/z_ivt_18/bugtracker/App.java
+++ src/main/java/edu/z_ivt_18/bugtracker/App.java
@@ -11,6 +11,28 @@ public final class App {
     private final Database database;
 
     public App() {
+        // Самое главное: темы.
+        // Если системная тема — Metal, то меняем её на Nimbus.
+        // В противном случае (Windows) оставляем как есть.
+        try {
+            String systemThemeName = UIManager.getSystemLookAndFeelClassName();
+
+            if (systemThemeName.endsWith("MetalLookAndFeel")) {
+                UIManager.LookAndFeelInfo[] themes = UIManager.getInstalledLookAndFeels();
+
+                for (UIManager.LookAndFeelInfo theme : themes) {
+                    if (theme.getName().equals("Nimbus")) {
+                        UIManager.setLookAndFeel(theme.getClassName());
+                        break;
+                    }
+                }
+            } else {
+                UIManager.setLookAndFeel(systemThemeName);
+            }
+        } catch (Exception ex) {
+            System.err.println("Failed to set look and feel: " + ex.getMessage());
+        }
+
         translations = ResourceBundle.getBundle("translations");
         settings = Preferences.userNodeForPackage(edu.z_ivt_18.bugtracker.App.class);
         mainWindow = new MainWindow(this);
